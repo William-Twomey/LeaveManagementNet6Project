9e2afb957546d8de43f8dc1748d8ebfe72325927
William_Twomey <irowt@mail.com>
2022-07-26 21:54:08 +0100

    Modified the CRUD screens and added datatables and swal.

---
+++ LeaveManagement.Web/ViewModels/Configurations/MapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.ViewModels.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagement.Web/ViewModels/LeaveTypeVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.ViewModels
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        
+        [Display (Name = "Leave Type")]
+        [Required]
+        public string Name { get; set; }
+        
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter Between 1 and 25")]
+        public int DefaultDays { get; set; }
+    }
+}
+
+
